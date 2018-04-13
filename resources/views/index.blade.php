8b90b3e4b25c8674d9ff307511bc159d7a8daf25
WebDesarrollo <37180933+webdesarrollo@users.noreply.github.com>
2018-04-13 15:16:30 -0300

    asset
    cambio URL::asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,8 @@
     <title>Salas | Bolivar</title>
     <link rel="icon" href="/img/rock-and-roll.png"/>
     <!-- Bootstrap -->
-    <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
-    <link href="{{ URL::asset('css/estilos.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('css/estilos.css') }}" rel="stylesheet" type="text/css" >
 </head>
     
 <body data-spy="scroll" data-target="#myScrollspy" data-offset="60">
