d5a1f947358737e28e7efab13b5747add1057d12
WebDesarrollo <37180933+webdesarrollo@users.noreply.github.com>
2018-04-13 15:39:08 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,8 +21,8 @@
     @include('principal.contacto')
     @include('principal.footer')
     
-    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
 </body>
 </html>
