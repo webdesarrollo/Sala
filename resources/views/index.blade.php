12888fe7afa9ae32a7d4278fe9d93b5b43ce63de
WebDesarrollo <37180933+webdesarrollo@users.noreply.github.com>
2018-04-15 14:29:42 -0300

    css js
    edito css y js para heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,10 @@
     <title>Salas | Bolivar</title>
     <link rel="icon" href="/img/rock-and-roll.png"/>
     <!-- Bootstrap -->
-    {!!Html::style('css/bootstrap.min.css')!!}
-    {!!Html::style('css/estilos.css')!!}
-    {!!Html::script('js/scrollreveal.min.js')!!}
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+    <link href="/css/estilos.css" rel="stylesheet" type="text/css" >
+    <script type="/js/scrollreveal.min.js"></script>
 </head>
     
 <body data-spy="scroll" data-target="#myScrollspy" data-offset="60">
@@ -19,9 +20,9 @@
     @yield('contenido')
     @include('principal.footer')
 
-    {!!Html::script('js/jquery.min.js')!!}
-    {!!Html::script('js/bootstrap.min.js')!!}
-    {!!Html::script('js/main.js')!!}
-    {!!Html::script('js/scroll.js')!!}
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
+    <script type="text/javascript" src="/js/scroll.js"></script>
 </body>
 </html>
