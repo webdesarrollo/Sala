6c6cb23f57e408552315fd2a9f32ef0f267eb55c
WebDesarrollo <37180933+webdesarrollo@users.noreply.github.com>
2018-04-19 15:40:45 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,20 +9,20 @@
     <title>Salas | Bolivar</title>
     <link rel="icon" href="/img/rock-and-roll.png"/>
     <!-- Bootstrap -->
-    {!!Html::style('css/bootstrap.min.css')!!}
-    {!!Html::style('css/estilos.css')!!}
-    {!!Html::script('js/scrollreveal.min.js')!!}
+    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css" >
+    <link href="/css/estilos.css" rel="stylesheet" type="text/css" >
+    <script src="/js/scrollreveal.min.js"></script>
 </head>
     
 <body data-spy="scroll" data-target="#myScrollspy" data-offset="60">
     
     @yield('contenido')
     @include('principal.footer')
-
-    {!!Html::script('js/jquery.min.js')!!}
-    {!!Html::script('js/bootstrap.min.js')!!}
-    {!!Html::script('js/main.js')!!}
-    {!!Html::script('js/scroll.js')!!}
-    {!!Html::script('js/modal.js')!!}
+    
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
+    <script type="text/javascript" src="/js/scroll.js"></script>
+    <script type="text/javascript" src="/js/modal.js"></script>
 </body>
 </html>
